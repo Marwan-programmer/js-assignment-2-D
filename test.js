8b2d64a23625830baaec73ecbfe27f06c62d42d9
marwan <marwan.alsubayi@gmail.com>
2021-04-20 18:24:53 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+const
+      user=prompt("enter a string");
+
+
+      if(user=="red"){
+
+        alert("apple");
+      }else if(user=="yellow"){
+        alert("banana");
+
+      }else if(user=="green"){
+        alert("grass");
+      }else if(user=="circle" || user=="rectangle" || user=="square"){
+        alert("shape"); 
+
+      }else{
+
+        alert("i don't know")
+      }
